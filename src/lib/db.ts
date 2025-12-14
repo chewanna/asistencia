a2c05b9a9ae09c054d6d180ac1d7f3edfdf1aac9
chewanna <salgadosanchezyael23@gmail.com>
2025-12-13 20:15:48 -0600

    Add files via upload

---
+++ src/lib/db.ts
@@ -0,0 +1,8 @@
+// src/lib/db.ts
+import { neon } from "@neondatabase/serverless";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL");
+}
+
+export const sql = neon(process.env.DATABASE_URL);
